0e242019062f627290bcc8325334bbe727391d92
20fut <231612@astanait.edu.kz>
2024-04-24 13:48:18 +0500

    1

---
+++ src/MyArrayList.java
@@ -0,0 +1,149 @@
+import Exceptions.ElementNotFoundException;
+
+public class MyArrayList<T extends Comparable<T>> implements MyList<T> {
+    private T[] arr;
+    private int size;
+
+    public MyArrayList() {
+        arr = (T[]) new Object[4];
+        size = 0;
+    }
+    private void increaseBuffer() {
+        T[] newArr = (T[]) new Object[arr.length * 2];
+        for (int i = 0; i < size; i++) {
+            newArr[i] = arr[i];
+        }
+        arr = newArr;
+    }
+
+    @Override
+    public void add(T item) {
+        if (size >= arr.length) {
+            increaseBuffer();
+        }
+        arr[size++] = item;
+    }
+
+    @Override
+    public void set(int index, T item) {
+        if (size >= arr.length) {
+            increaseBuffer();
+        }
+        checkIndex(index);
+        arr[index] = item;
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size)
+            throw new IndexOutOfBoundsException("index not correct");
+    }
+
+    @Override
+    public void addFirst(T item) {
+        set(0,item);
+    }
+
+    @Override
+    public void addLast(T item) {
+        set(arr.length-1,item);
+    }
+
+    @Override
+    public T get(int index) {
+        checkIndex(index);
+        return arr[index];
+    }
+
+    @Override
+    public T getFirst() {
+        return get(0);
+    }
+
+    @Override
+    public T getLast() {
+        return get(arr.length-1);
+    }
+
+    @Override
+    public void remove(int index) {
+        checkIndex(index);
+        for(int i = index;i<size-1;i++){
+            arr[i]=arr[i+1];
+        }
+    }
+
+    @Override
+    public void removeFirst() {
+        remove(0);
+    }
+
+    @Override
+    public void removeLast() {
+        remove(size-1);
+    }
+
+    @Override
+    public void sort() {
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = 0; j < size - 1 - i; j++) {
+                if (arr[j].compareTo(arr[j + 1]) > 0) {
+                    T temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    @Override
+    public int indexOf(Object object) throws ElementNotFoundException {
+        boolean check =exists(object);
+        if(check){
+            for (int i = 0; i < size; i++) {
+                if (arr[i].equals(object)) {
+                    return i;
+                }
+            }
+        }
+        else 
+            throw new ElementNotFoundException("Element not found in the list");
+    }
+
+    @Override
+    public int LastIndexOf(Object object) {
+        return size-1;
+    }
+
+    @Override
+    public boolean exists(Object object) throws NullPointerException {
+        if (object == null) {
+            throw new NullPointerException("Search object cannot be null");
+        }
+        for (int i = 0; i < size; i++) {
+            if (arr[i].equals(object)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public T[] toArray() {
+        T[] newArray = (T[]) new Object[size];
+        for (int i = 0; i < size; i++) {
+            newArray[i] = arr[i];
+        }
+        return newArray;
+    }
+
+    @Override
+    public void clear() {
+        size=0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+}
