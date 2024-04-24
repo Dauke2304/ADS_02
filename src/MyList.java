647354aa16a561c463f269207c4b27b4d15e7a20
20fut <231612@astanait.edu.kz>
2024-04-24 12:59:43 +0500

    1

---
+++ src/MyList.java
@@ -0,0 +1,20 @@
+public interface MyList<T> extends Iterable<T> {
+    void add(T item);
+    void set(int index, T item);
+    void add(int index, T item);
+    void addFirst(T item);
+    void addLast(T item);
+    T get(int index);
+    T getFirst();
+    T getLast();
+    void remove(int index);
+    void removeFirst();
+    void removeLast();
+    void sort();
+    int index0f(Object object);
+    int LastIndex0f(Object object);
+    boolean exists(Object object);
+    public Object[] toArray();
+    void clear();
+    int size();
+}
