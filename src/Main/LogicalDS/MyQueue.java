2d605e35fac6e17d088be1c6ced451cd357289c3
20fut <231612@astanait.edu.kz>
2024-04-26 11:40:08 +0500

    Stack

--- src/Main/LogicalDS/MyQueue.java
+++ src/Main/LogicalDS/MyQueue.java
@@ -1,4 +1,42 @@
 package Main.LogicalDS;
 
-public class MyQueue {
+import Main.LinkedLists.MyLinkedList;
+
+public class MyQueue<T extends Comparable<T>> {
+    private MyLinkedList<T> myQueue;
+
+    public MyQueue(){
+        myQueue = new MyLinkedList<>();
+    }
+    public T push(T item){
+        myQueue.addLast(item);
+        return item;
+    }
+    public boolean isEmpty() {
+        return myQueue.size() == 0;
+    }
+    public T pop() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Queue is empty");
+        }
+        T poppedItem = myQueue.getFirst();
+        myQueue.removeFirst();
+        return poppedItem;
+    }
+    public T peek() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Queue is empty");
+        }
+        return myQueue.getFirst();
+    }
+    public int size() {
+        return myQueue.size();
+    }
+    public void clear() {
+        myQueue.clear();
+    }
+    public void printQueue() {
+        myQueue.PrintLinkedList();
+    }
+
 }
